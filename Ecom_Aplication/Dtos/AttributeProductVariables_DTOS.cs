1dbeb3fa6a5535fa44ad0b49d5c29e5a0e74e6ef
Endersonlopez <endersonlopez8@hotmail.com>
2026-06-17 02:14:21 -0600

    Domain and DTO of tables 12 to 20

---
+++ Ecom_Aplication/Dtos/AttributeProductVariables_DTOS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecom_Aplication.Dtos
+{
+    public class AttributeProductVariables_DTOS
+    {
+        public int? attributeProductVariableId { get; set; }
+        public int attributeProductVariableProductVariableId { get; set; }
+        public int attributeProductVariableAttributeProductId { get; set; }
+        public string? attributeProductVariableValue { get; set; }
+        public int attributeProductVariableCreatorId { get; set; }
+        public DateTime? attributeProductVariableCreationDate { get; set; }
+        public int attributeProductVariableModificatorId { get; set; }
+        public DateTime? attributeProductVariableModificationDate { get; set; }
+        public bool? attributeProductVariableStatusId { get; set; }
+    }
+}
---
+++ Ecom_Domain/Currencies.cs
@@ -0,0 +1,22 @@
+﻿using Ecom_Domain;
+using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace Ecom_Domain
+{
+    public class Currencies
+    {
+        public int? currencyId { get; set; }
+        public string? currencyName { get; set; }
+        public string? currencyISO { get; set; }
+        public int currencyCode { get; set; }
+        public string? currencyDescription { get; set; }
+        public int currencyCreatorId { get; set; }
+        public DateTime? currencyCreationDate { get; set; }
+        public int currencyModificatorId { get; set; }
+        public DateTime? currencyModificationDate { get; set; }
+        public bool? currencyStatusId { get; set; }
+    }
+}
